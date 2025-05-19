880684cdfd15644a0f91f88fe659a5886015d06c
Mike Lake <Mike.Lake@uts.edu.au>
2025-05-19 12:47:24 +1000

    Added LD_RUN_PATH

--- pbs/swig_compile_pbs.sh
+++ pbs/swig_compile_pbs.sh
@@ -2,11 +2,11 @@
 
 # Uses the PBS files pbs_ifl.h and pbs.i to create pbs.py, pbs_wrap.c and _pbs.so.
 
-conf="/etc/pbs.conf"  # PBS configuration file
-
 #############################
 # Set your configuration here
 #############################
+
+conf="/etc/pbs.conf"  # PBS configuration file
 
 # Example: If using my laptop.
 PYTHON_INCL="/usr/include/python3.8"
@@ -27,6 +27,8 @@ fi
 # The PBS config file must be sourced to provide $PBS_EXEC.
 . $conf
 
+export LD_RUN_PATH=$PBS_EXEC/lib
+
 # Running swig creates pbs.py and pbs_wrap.c
 $SWIG_EXEC -I$PBS_EXEC/include -python pbs.i
 
@@ -36,8 +38,18 @@ if [ $? -ne 0 ]; then
 fi
 
 # Running gcc creates _pbs.so
-gcc -shared -fPIC -I$PYTHON_INCL -I$PBS_EXEC/include pbs_wrap.c $PBS_EXEC/lib/libpbs.so \
-    -o _pbs.so -L/lib -lcrypto -lssl
+#gcc -shared -fPIC -I$PYTHON_INCL -I$PBS_EXEC/include pbs_wrap.c $PBS_EXEC/lib/libpbs.so \
+#    -o _pbs.so -L/lib -lcrypto -lssl
+
+# Compiling
+gcc -c -shared -fpic -I$PYTHON_INCL -I$PBS_EXEC/include pbs_wrap.c -o pbs_wrap.o 
+
+# Linking
+# Not sure if we need to also add -L/lib
+gcc -shared -fpic -L/opt/pbs/lib \
+    $PBS_EXEC/lib/libpbs.so pbs_wrap.o \
+    -lpthread -lcrypto -lssl \
+    -o _pbs.so
 
 # It does not need to be executable.
 chmod ugo-x _pbs.so
